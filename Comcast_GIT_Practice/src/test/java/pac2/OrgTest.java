81e25db23a7d45c9cc5b6900b111d3c2b3628d89
seleniumadv <pranavverma19981998@gmail.com>
2024-07-30 15:38:08 +0530

    add modification

--- Comcast_GIT_Practice/src/test/java/pac2/OrgTest.java
+++ Comcast_GIT_Practice/src/test/java/pac2/OrgTest.java
@@ -6,6 +6,16 @@ public class OrgTest {
 
 	@Test
 	public void createOrgTest() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
 		System.out.println("execute createOrgTest");
 	}
 	
